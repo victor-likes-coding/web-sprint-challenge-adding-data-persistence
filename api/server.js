0bd1ecdfbb669fc8eae5b65017949581f7e87fe8
Victor Tran <victor-likes-coding@tempestas.io>
2023-03-30 03:44:18 -0400

    import projects router into server and use it

--- api/server.js
+++ api/server.js
@@ -1,1 +1,14 @@
 // build your server here and require it from index.js
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const projectsRouter = require('./projects/projects-router');
+const server = express();
+
+server.use(express.json());
+server.use(helmet());
+server.use(cors());
+
+server.use('/api/projects', projectsRouter);
+
+module.exports = server;
